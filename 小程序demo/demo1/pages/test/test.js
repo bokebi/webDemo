5a7bab96e918326236047f371db14f7b16ea49c5
bokebi520 <bokebi520@qq.com>
2017-08-23 09:25:35 +0800

    整理一些demo文件

--- 小程序demo/demo1/pages/test/test.js
+++ 小程序demo/demo1/pages/test/test.js
@@ -1,75 +1,15 @@
-var helloData = {
-    name: "Zhangsan",
-    
-    message: "数据绑定",
-    
-    array: [1,2,3,4,5],
-    
-    view: 'APP',
-    
-    staffA: { 
-        firstName:'张',
-        lastName: '三'
-    },
-    staffB: { 
-        firstName:'李',
-        lastName: '四'
-    },
-    staffC: { 
-        firstName:'王',
-        lastName: '五'
-    },
-
-    count: 123,
-
-    a: 1,
-    b: 2,
-    c: 3,
-
-    length: 7,
-
-    object:{key: '您好：'},
-    arr: ['李四'],
-
-    zero: 0,
-
-    obj1: {
-      a: 1,
-      b: 2
-    },
-    obj2: {
-      c: 3,
-      d: 4
-    },
-
-array1: [
-    {message:'列表'},
-    {message:'渲染'}
-], 
-array2: [
-    {message:'自定义'},
-    {message:'列表渲染'}
-],     
-
-}
-
-// 注册一个页面
 Page({
-    data: helloData,
-
-    changeName: function(e){
-        //发送数据改变视图
-        this.setData({
-            name: "张三"
-        })
-    },
-
-    add: function(e){
-        this.setData({
-            count: this.data.count + 1
-        })
-    },
-    tapName: function(event){
-        console.log(event);
-    }
+  data: {
+      slider: [
+        {picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000rVobR3xG73f.jpg'},
+        {picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000j6Tax0WLWhD.jpg'},
+        {picUrl: 'http://y.gtimg.cn/music/photo_new/T003R720x288M000000a4LLK2VXxvj.jpg'},
+      ],
+      swiperCurrent: 0,
+  },
+  swiperChange: function(e){
+      this.setData({
+          swiperCurrent: e.detail.current
+      })
+  }
 })
